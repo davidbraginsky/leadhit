51fa60473edf6269d6702584426db18572bb5f26
David <braginsky.david@yandex.ru>
2022-06-25 22:26:04 +0300

    added vuex to store visits data

--- src/views/AnalyticsView.vue
+++ src/views/AnalyticsView.vue
@@ -18,6 +18,14 @@ export default {
     }
   },
   mounted() {
+    const visitsData = this.$store.state.visitsData;
+    const formatedVisitsData = visitsData.map((item) => {
+      const newObj = {
+        date: new Date(item.date).getTime(),
+        visits: item.visits,
+      };
+      return newObj;
+    });
     let root = am5.Root.new(this.$refs.chartdiv);
 
     root.setThemes([am5themes_Animated.new(root)]);
@@ -30,39 +38,6 @@ export default {
         maxTooltipDistance: 0,
       })
     );
-
-    // Define data
-    var data = [
-      { date: new Date("2020-07-01").getTime(), visits: 213 },
-      { date: new Date("2020-07-02").getTime(), visits: 249 },
-      { date: new Date("2020-07-03").getTime(), visits: 179 },
-      { date: new Date("2020-07-04").getTime(), visits: 170 },
-      { date: new Date("2020-07-05").getTime(), visits: 184 },
-      { date: new Date("2020-07-06").getTime(), visits: 202 },
-      { date: new Date("2020-07-07").getTime(), visits: 198 },
-      { date: new Date("2020-07-08").getTime(), visits: 168 },
-      { date: new Date("2020-07-09").getTime(), visits: 176 },
-      { date: new Date("2020-07-10").getTime(), visits: 171 },
-      { date: new Date("2020-07-11").getTime(), visits: 190 },
-      { date: new Date("2020-07-12").getTime(), visits: 154 },
-      { date: new Date("2020-07-13").getTime(), visits: 246 },
-      { date: new Date("2020-07-14").getTime(), visits: 250 },
-      { date: new Date("2020-07-15").getTime(), visits: 227 },
-      { date: new Date("2020-07-16").getTime(), visits: 140 },
-      { date: new Date("2020-07-17").getTime(), visits: 170 },
-      { date: new Date("2020-07-18").getTime(), visits: 125 },
-      { date: new Date("2020-07-19").getTime(), visits: 106 },
-      { date: new Date("2020-07-20").getTime(), visits: 207 },
-      { date: new Date("2020-07-21").getTime(), visits: 222 },
-      { date: new Date("2020-07-22").getTime(), visits: 198 },
-      { date: new Date("2020-07-23").getTime(), visits: 204 },
-      { date: new Date("2020-07-24").getTime(), visits: 213 },
-      { date: new Date("2020-07-25").getTime(), visits: 145 },
-      { date: new Date("2020-07-26").getTime(), visits: 166 },
-      { date: new Date("2020-07-27").getTime(), visits: 163 },
-      { date: new Date("2020-07-28").getTime(), visits: 135 },
-      { date: new Date("2020-07-29").getTime(), visits: 45 },
-    ];
 
     var yAxis = chart.yAxes.push(
       am5xy.ValueAxis.new(root, {
@@ -107,7 +82,7 @@ export default {
       series.strokes.template.set("strokeWidth", 2);
 
       series.get("tooltip").label.set("text", "[bold]{name}[/]\n{valueX.formatDate()}: {valueY}");
-      series.data.setAll(data);
+      series.data.setAll(formatedVisitsData);
       series.set("fill", am5.color("#9955fc")); // set Series color to green
       series.set("stroke", am5.color("#9955fc")); // set Series color to green
     }
