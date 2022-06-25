82c72ae7a47551a292295ad977f7e9b4123bef42
David <braginsky.david@yandex.ru>
2022-06-25 21:46:05 +0300

    added functionality to send request to server

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <h1>LeadHit</h1>
     <form class="id-form">
-      <MDBInput class="mb-3" color="danger" counter :maxlength="24" label="ID сайта" v-model="userInput" />
-      <span ref="errorRef" class="form-text"> Site ID must be 24 characters </span>
+      <MDBInput @input="inputHandler" class="mb-3" color="danger" counter :maxlength="24" label="ID сайта" v-model="userInput" />
+      <span ref="errorRef" class="form-text"> id сайта должен содержать 24 символа </span>
       <MDBBtn @click.prevent="submitHandler" type="submit" color="primary" rounded>Войти</MDBBtn>
     </form>
   </div>
@@ -22,15 +22,46 @@ export default {
     const userInput = ref("");
     const errorRef = ref(null);
 
-    const submitHandler = () => {
-      if (userInput.value.length !== 24) {
-        console.dir(errorRef.value.classList.add("form-text--active"));
-      } else if (userInput.value.length === 24) {
-        console.log("user input is correct length");
+    const showErrorMessage = () => {
+      errorRef.value.classList.add("form-text--active");
+    };
+
+    const hideErrorMessage = () => {
+      errorRef.value.classList.remove("form-text--active");
+    };
+
+    const sendRequestToServer = async () => {
+      const response = await fetch("https://track-api.leadhit.io/client/test_auth", {
+        method: "GET",
+        headers: {
+          "Api-Key": "5f8475902b0be670555f1bb3:eEZn8u05G3bzRpdL7RiHCvrYAYo",
+          "Leadhit-Site-Id": userInput.value,
+        },
+      });
+      const reply = await response.json();
+      const { message } = reply;
+      if (message === "ok") {
+        console.log("success");
+      } else {
+        console.log("something went wrong");
       }
     };
 
-    return { userInput, submitHandler, errorRef };
+    const submitHandler = () => {
+      if (userInput.value.length !== 24) {
+        showErrorMessage();
+      } else if (userInput.value.length === 24) {
+        sendRequestToServer();
+      }
+    };
+
+    const inputHandler = () => {
+      if (errorRef.value.classList.contains("form-text--active")) {
+        hideErrorMessage();
+      }
+    };
+
+    return { userInput, submitHandler, errorRef, inputHandler };
   },
 };
 </script>
